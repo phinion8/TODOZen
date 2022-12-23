298c7c15fed4df47a0ed602a6cac408aa73ee61f
phinion <priyanshu831780@gmail.com>
2022-12-23 15:04:00 +0530

    commit 3

---
+++ app/src/main/java/com/dayscode/todojpc/ToDoApplication.kt
@@ -0,0 +1,7 @@
+package com.dayscode.todojpc
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication: Application()
---
+++ app/src/main/java/com/dayscode/todojpc/di/DatabaseModule.kt
@@ -0,0 +1,28 @@
+package com.dayscode.todojpc.di
+
+import android.content.Context
+import androidx.room.Room
+import com.dayscode.todojpc.data.ToDoDatabase
+import com.dayscode.todojpc.util.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, ToDoDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ToDoDatabase) = database.toDoDao()
+
+}
